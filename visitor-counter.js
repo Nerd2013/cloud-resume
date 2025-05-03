e2ae800e7fffcc8acd7b0c4d3972f5f63f0dfe08
Nerd2013 <madhur.bhinda@gmail.com>
2025-05-03 16:38:42 -0400

    Fix Content Security Policy by rmeoving the js content outside the html

--- visitor-counter.js
+++ visitor-counter.js
@@ -1,12 +1,13 @@
 async function updateVisitorCount() {
   const counterEl = document.getElementById("visitor-count");
+
   try {
     const response = await fetch("https://ka14bycopi.execute-api.us-east-1.amazonaws.com/default/VisitorCounterFunction");
     const data = await response.json();
     counterEl.innerText = `👁️ Visitors: ${data.count}`;
   } catch (err) {
     console.error("Visitor counter error:", err);
-    if (counterEl) counterEl.innerText = "👁️ Visitors: unavailable";
+    counterEl.innerText = "👁️ Visitors: unavailable";
   }
 }
 
